71c5b4b5399a69378a8f0f8cdecca6af73d96969
Swapnil Pande <swapnilpande96@gmail.com>
2019-01-04 03:35:11 +0530

    Added ES6 example

---
+++ ES6/script.js
@@ -0,0 +1,83 @@
+class TodoList {
+  constructor(root) {
+    // Local state management object
+    this.state = [];
+
+    // UI variables
+    this.root = root;
+    this.form = root.querySelector('.form-wrapper');
+    this.addbtn = this.form.querySelector('.addBtn');
+    this.input = this.form.querySelector('#todo-input');
+    this.help = root.querySelector('.help');
+    this.ul = root.querySelector('ul');
+    // Lookup object containing the uid as key and li element as object
+    this.items = {}
+
+    // event handlers
+    this.addbtn.addEventListener('click', e => {
+      e.preventDefault()
+      const { value } = this.input;
+      if (value === '') {
+        alert("For Gods Sake just type something before adding anything!");
+      } else {
+        this.input.value = '';
+        this.addItem(value);
+      }
+    })
+
+    this.ul.addEventListener('click', e => {
+      if (e.target.classList.contains('close')) {
+        const id = e.target.parentElement.getAttribute('data-delete-id')
+        if (id) {
+          this.removeItem(id)
+        }
+      }
+    })
+  }
+
+  addItem(item) {
+    const id = this.state.length + 1;
+    // Updating the local state 
+    this.state = this.state.concat({ item, id })
+
+    // UI logic
+    this.toggleEmptyState()
+
+    const li = document.createElement('li');
+    li.setAttribute('data-delete-id', id);
+    const text = document.createTextNode(item);
+    li.appendChild(text);
+
+    const span = document.createElement("span");
+    const txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    this.ul.appendChild(li)
+    this.items[id] = li
+  }
+
+  removeItem(id) {
+    const uid = parseInt(id, 10)
+    // Updating the local state 
+    this.state = this.state.filter(item => item.id !== uid)
+
+    // UI logic
+    const li = this.items[uid]
+    this.ul.removeChild(li)
+    this.toggleEmptyState()
+  }
+
+  toggleEmptyState() {
+    const element = document.getElementsByClassName('help')[0];
+    if (this.state.length > 0) {
+      element.style.display = 'none';
+    } else {
+      element.style.display = 'block';
+    }
+  }
+}
+
+const root = document.getElementById('root')
+new TodoList(root)
