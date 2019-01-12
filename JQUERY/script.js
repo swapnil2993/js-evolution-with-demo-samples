6531a77556c7d72d5baed33abba170aa91c2419e
Swapnil Pande <swapnilpande96@gmail.com>
2019-01-12 14:11:39 +0530

    Added jquery sample.

--- JQUERY/script.js
+++ JQUERY/script.js
@@ -1,40 +1,31 @@
 // Global declaration of list 
-var myNodelist = document.getElementsByTagName("li");
-
-function removeItem(i) {
-  var element = document.querySelectorAll("[data-delete-id=" + "'" + i + "'" + "]")[0];
-  element.parentNode.removeChild(element);
-  toggleEmptyState()
+function removeItem() {
+  $(this).closest('li').remove()
+  toggleEmptyState();
 }
 
 function toggleEmptyState() {
-  var element = document.getElementsByClassName('help')[0];
-  if (myNodelist.length > 0) {
-    element.style.display = 'none';
+  debugger
+  if ($("li").length > 0) {
+    $('.help').css('display', 'none');
   } else {
-    element.style.display = 'block';
+    $('.help').css('display', 'block');
   }
 }
 
 // Create a new list item when clicking on the "Add" button
 function newItem() {
-  var newIter = myNodelist.length + 1;
-  var li = document.createElement("li");
-  li.setAttribute('data-delete-id', newIter)
-  var inputValue = document.getElementById("todo-input").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
+  var newIter = $("li").length + 1;
+  var inputValue = $("#todo-input").val();
   if (inputValue === '') {
     alert("For Gods Sake Just type something before adding anything!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    var li = "<li data-delete-id='" + newIter + "'>" + inputValue + "</li>";
+    $('#myUL').append(li);
+    $("#todo-input").val("");
+    var span = "<span class='close'>\u00D7</span>";
+    $("[data-delete-id='" + newIter + "']").append(span);
+    $('.close').off('click').on('click', removeItem);
   }
-  document.getElementById("todo-input").value = "";
-  var span = document.createElement("span");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  span.onclick = removeItem.bind(null, newIter);
-  li.appendChild(span);
   toggleEmptyState()
 }
